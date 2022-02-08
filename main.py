c6d3939ecc02723aef27f03824560bd36602426d
Jason <xu.zesheng.sh@gmail.com>
2022-02-08 04:27:01 -0600

    Connected to Wordle site using selenium, functional

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 Author: Zesheng Xu
 Date: Feb 6 2022
 Functions:
+main() - the driver function
+load_list() - from the english dictionary, load all 5 letter word into an array
+update_list() - remove unfitting words from the words list according to known knowledge
+count_occurance() - count how many times a letter appeared in a letter_object list
+letter_object_set_Value() - updating value of the letter_object array smartly
 
 """
+import time
+
+from selenium.webdriver.common.by import By
 
 from letter import letter_object
 from english_words import english_words_lower_alpha_set
 import numpy as np
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 def main():
     """
     Author: Zesheng Xu
@@ -17,7 +27,22 @@ def main():
 
     :return: none
     """
-    word = "skill"
+
+    #launch wordle website
+    driver = webdriver.Chrome("chromedriver.exe")
+    driver.get("https://www.powerlanguage.co.uk/wordle/")
+
+    # close  the introduction page
+    time.sleep(1)
+
+    Elem = driver.find_element_by_tag_name('html')
+
+    Elem.click()
+
+
+
+
+    word = "frame"
     word_list = load_list()
 
 
@@ -29,27 +54,42 @@ def main():
         if(len(word_list) > 0):
             guess = np.random.choice(word_list)
             guessed.append(guess)
-            if(guess == word):
-                print("Success on %s try, and the word is: %s"% ( i, guess))
-                return 0;
-            else:
 
-                for j in range(0,len(word)):
-                    if(guess[j] == word[j]):
-                        print('%s at location %s confirmed'% (guess[j],j))
-                        temp_letter = letter_object(guess[j], "confirmed")
-                        certain = letter_object_set(certain, temp_letter, j)
+            # send guess to wordle.com
+            Elem.send_keys(guess)
+            Elem.send_keys(Keys.ENTER)
 
 
-                    elif (guess[j] in word):
-                        print('%s at location %s exists'% (guess[j], j))
-                        temp_letter = letter_object(guess[j], "exist")
-                        certain = letter_object_set(certain, temp_letter, j)
+            time.sleep(2)
+
+            combined_list = get_row_result(driver,i) # a temp tuple
+            excluded += combined_list[0]
+            certain=combined_list[1]
 
 
-                    else:
-                        if(guess[j] not in excluded):
-                            excluded.append(guess[j])
+
+            # if(guess == word):
+            #     print("Success on %s try, and the word is: %s"% ( i, guess))
+            #     print(guessed)
+            #     return 0;
+            # else:
+            #
+            #     for j in range(0,len(word)):
+            #         if(guess[j] == word[j]):
+            #             print('%s at location %s confirmed'% (guess[j],j))
+            #             temp_letter = letter_object(guess[j], "confirmed")
+            #             certain = letter_object_set_Value(certain, temp_letter, j)
+            #
+            #
+            #         elif (guess[j] in word):
+            #             print('%s at location %s exists'% (guess[j], j))
+            #             temp_letter = letter_object(guess[j], "exist")
+            #             certain = letter_object_set_Value(certain, temp_letter, j)
+            #
+            #
+            #         else:
+            #             if(guess[j] not in excluded):
+            #                 excluded.append(guess[j])
             word_list = update_list(word_list, excluded, certain)
         else:
             print("This word is not within our dictionary")
@@ -126,7 +166,7 @@ def update_list(list, excluded, certain):
                             break
 
         count += 1
-        print (count, len(updated_list))
+        #print (count, len(updated_list))
         if(count < 0):
             print("Count is negative")
             break
@@ -149,7 +189,7 @@ def count_occurance(letter, letter_list):
             count += 1
     return count
 
-def letter_object_set(certain_list, new_letter, index):
+def letter_object_set_Value(certain_list, new_letter, index):
     """
     Author: Zesheng Xu
     Date: Feb 7 2022
@@ -175,6 +215,39 @@ def letter_object_set(certain_list, new_letter, index):
     certain_list[index] = new_letter
     return certain_list;
 
+def get_row_result(webdriver,index):
+    """
+    :param webdriver: The wordle website
+    :param webdriver: The Row number to get
+    :return: list - a letter_object list containing the result of our previous entrance - including the state of the letter
+    :return: to_exclude - letters to exclude from the gusses
+    """
+    to_exclude= []
+    list = [None, None, None, None, None]
+    # locating the game-row through the series of shadow roots - I hate this
+    host = webdriver.find_element(By.TAG_NAME, "game-app")
+    game = webdriver.execute_script("return arguments[0].shadowRoot.getElementById('game')",host)
+    board = game.find_element(By.ID, "board")
+    rows = webdriver.execute_script("return arguments[0].getElementsByTagName('game-row')", board)
+    row = webdriver.execute_script("return arguments[0].shadowRoot.querySelector(""'.row').innerHTML",rows[index])
+
+    bs_text = BeautifulSoup(row, 'html.parser')
+
+    count = 0
+    for tile in bs_text.findAll('game-tile'): # goes through each tile in the row
+        letter = tile.get('letter')
+        eval = tile.get('evaluation')
+
+        if(eval == "present"):
+            temp = letter_object(letter,"exist")
+            list[count] = temp
+        elif (eval == "correct"):
+            temp = letter_object(letter, "confirmed")
+            list[count] = temp
+        elif (eval == "absent"):
+            to_exclude.append(letter)
+        count += 1
+    return to_exclude,list
 
 
 if __name__ == "__main__":
